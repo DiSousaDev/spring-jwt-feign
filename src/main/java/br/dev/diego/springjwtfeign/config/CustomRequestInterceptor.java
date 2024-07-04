11ba473c31a6f971392f51ef470e089bacf19618
Diego Sousa <me@diego.dev.br>
2024-07-04 01:05:59 -0300

    teste interceptor

--- src/main/java/br/dev/diego/springjwtfeign/config/CustomRequestInterceptor.java
+++ src/main/java/br/dev/diego/springjwtfeign/config/CustomRequestInterceptor.java
@@ -4,10 +4,14 @@ import br.dev.diego.springjwtfeign.services.JwtTokenService;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
-public class FeignRequestInterceptor implements RequestInterceptor {
+import java.sql.SQLOutput;
+import java.util.Collection;
+import java.util.Map;
+
+@Configuration
+public class CustomRequestInterceptor implements RequestInterceptor {
 
     @Autowired
     private JwtTokenService jwtTokenService;
@@ -15,6 +19,16 @@ public class FeignRequestInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate requestTemplate) {
         System.out.println("Interceptando requisição");
+
+        Map<String, Collection<String>> headers = requestTemplate.headers();
+
+        headers.forEach((s, strings) -> {
+            System.out.println("Header Name " + s);
+            strings.forEach(s1 -> {
+                System.out.println("Values " + s1);
+            });
+        });
+
         String token = jwtTokenService.getToken();
         System.out.println("Token: " + token);
         requestTemplate.header("Authorization", "Bearer " + token);
--- src/main/java/br/dev/diego/springjwtfeign/services/FeignClientMcc.java
+++ src/main/java/br/dev/diego/springjwtfeign/services/FeignClientMcc.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @FeignClient(value = "mcc", url = "https://mcc-production.up.railway.app")
 public interface FeignClientMcc {
 
-    @RequestMapping(value = "/casais/{id}")
+    @RequestMapping(value = "/casais/{id}", headers = "Teste-HEader=Teste-HeaderValue")
     CasalDTO getCasal(@PathVariable("id") Long id);
 
 }
